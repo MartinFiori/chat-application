51c89090895b091cb654072b6b2889664bb1ec6f
Martin Fiori <martin.fiori@jphlions.com>
2024-01-15 14:58:39 -0300

    CHORE: agregando jsonwebtoken

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const { compareHash, createHash } = require("../utils/hash");
+const { areValuesEmpty } = require("../utils/areValuesEmpty.js");
 const {
   successMessage,
   errorMessage,
@@ -9,16 +10,21 @@ const users = [];
 
 function register(req, res) {
   const { email, password } = req.body;
-  const user_found = users.find((user) => user.email === email);
+  const [trimmedEmail, trimmedPassword] = [email.trim(), password.trim()];
+  if (areValuesEmpty([trimmedEmail, trimmedPassword])) {
+    return res.send(errorMessage("Please complete all inputs"));
+  }
+  const user_found = users.find((user) => user.email === trimmedEmail);
   if (user_found)
     return res.send(
       errorMessage("Email is already taken! Please choose another"),
     );
   users.push({
-    email,
-    password: createHash(password),
+    email: trimmedEmail,
+    password: createHash(trimmedPassword),
   });
   res.send(successMessage("User registered successfully"));
 }
 
-module.exports = { register };
+function logIn(req, res) {}
+module.exports = { register, logIn };
